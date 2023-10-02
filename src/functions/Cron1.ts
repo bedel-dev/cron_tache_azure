3422672c0c55117a6eb151d8a6b00c4db191bd13
bedel-dev <bedelcoulibaly8@gmail.com>
2023-10-02 14:47:01 +0000

    push get message send

--- src/functions/Cron1.ts
+++ src/functions/Cron1.ts
@@ -1,7 +1,21 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
+import axios from 'axios';
+import { API_BASE_URL } from '../../config';
 
 export async function Cron1(myTimer: Timer, context: InvocationContext): Promise<void> {
     context.log('Timer function processed request.');
+        // POST vers une URL
+        try {
+            const response = await axios.post(API_BASE_URL+'/v1.4/phone_messages/send.json', { /* Données à envoyer */ });
+            console.log('Réponse de la requête POST :', response.data);
+        } catch (error) {
+            console.error('Erreur lors de la requête POST :', error);
+        }
+    
+        if (myTimer.isPastDue) {
+            context.log("La fonction s'est exécutée en retard à cause d'une longue exécution précédente.");
+        }
+        context.log('Fonction exécutée à :', new Date().toISOString());
 }
 
 app.timer('Cron1', {

b6b8022a17931fe830cfcc0dcc23006c0ff82745
bedel-dev <bedelcoulibaly8@gmail.com>
2023-10-02 15:10:23 +0000

    traitement_en_masse

---
+++ src/functions/Cron3.ts
@@ -0,0 +1,21 @@
+import { app, InvocationContext, Timer } from "@azure/functions";
+import axios from 'axios';
+import { API_BASE_URL } from '../../config';
+export async function Cron3(myTimer: Timer, context: InvocationContext): Promise<void> {
+    try {
+        const response = await axios.post(API_BASE_URL+'/v1.4/agences/traitMasse.json', { /* Données à envoyer */ });
+        context.log('Réponse de la requête POST :', response.data);
+    } catch (error) {
+        context.error('Erreur lors de la requête POST :', error);
+    }
+
+    if (myTimer.isPastDue) {
+        context.log("La fonction s'est exécutée en retard à cause d'une longue exécution précédente.");
+    }
+    context.log('Fonction exécutée à :', new Date().toISOString());
+}
+
+app.timer('Cron3', {
+    schedule: '0 */1 * * * *',
+    handler: Cron3
+});
